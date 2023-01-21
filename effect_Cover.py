21ac870bc3be5bc5f2dd54f0caf26c8fea4dabb8
leminhuit <79295048+leminhuit@users.noreply.github.com>
2023-01-21 11:21:55 +0700

    Update effect_Cover.py

--- effect_Cover.py
+++ effect_Cover.py
@@ -4,6 +4,10 @@ import numpy as np;
 # Buoc 1 : Doc anh tu file
 input = cv2.imread('./Images/image1.png')
 input2 = cv2.imread('./Images/image2.png')
+h, w, _ = input2.shape
+
+# Buoc 2 : Resize the image
+input = cv2.resize(input, (w, h))
 
 view = input2.copy()
 H = input.shape[1]
